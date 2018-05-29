4027b411ac1d6ff4e03d977e8491d82cac4626b0
Ann <user@A34COMP1>
2018-05-29 15:59:45 +0300

    create project

---
+++ Library/Library/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book
+    {
+        public string Name { set; get; }
+        public string Author { set; get; }
+        public int Year { set; get; }
+        enum Genre { fantastic, horror, comedy, western, detective, drama, melodrama, action, historic, wars }
+        public string imgPath { set; get; }
+        public string filePath { set; get; }
+
+        //public override string ToString()
+        //{
+        //    return Name;
+        //}
+    }
+}
