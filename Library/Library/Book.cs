920ea3528ff3163c4e19239b913d220326c9c240
Ann <user@A34COMP1>
2018-05-29 17:48:59 +0300

    Books add

--- Library/Library/Book.cs
+++ Library/Library/Book.cs
@@ -6,18 +6,15 @@ using System.Threading.Tasks;
 
 namespace Library
 {
+    public enum Genre { fantastic, horror, comedy, western, detective, drama, melodrama, action, historic, wars }
     public class Book
     {
         public string Name { set; get; }
-        public string Author { set; get; }
+        public string Author {get; set; }
         public int Year { set; get; }
-        enum Genre { fantastic, horror, comedy, western, detective, drama, melodrama, action, historic, wars }
+        public Genre Genre { get; set; }
         public string imgPath { set; get; }
         public string filePath { set; get; }
 
-        //public override string ToString()
-        //{
-        //    return Name;
-        //}
     }
 }
--- Library/Library/MainWindow.xaml.cs
+++ Library/Library/MainWindow.xaml.cs
@@ -20,9 +20,22 @@ namespace Library
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Book> book = new List<Book>();
         public MainWindow()
         {
             InitializeComponent();
+            book.Add(new Book() { Name = "Eleanor Oliphant Is Completely Fine", Author = " Gail Honeyman", Year= 2018,Genre=Genre.action,imgPath = @"C:\Users\user.STEP.001\Desktop\EO.jpg" });
+            book.Add(new Book() { Name = "The Hitchhiker's Guide to the Galaxy ", Author = "Douglas Adams", Year = 2001, Genre = Genre.action, imgPath = @"C:\Users\user.STEP.001\Desktop\386162.jpg" });
+            book.Add(new Book() { Name = "Diary of Tracy Bicker", Author = "Jacqueline Wilson", Year = 2002, Genre = Genre.action, imgPath = "C:/Users/user.STEP.001/Desktop/1024056_0.jpg" });
+            lv.ItemsSource = book;
+            MessageBox.Show(lv.Items.Count.ToString());
+        }
+
+        private void view_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
+
+//
